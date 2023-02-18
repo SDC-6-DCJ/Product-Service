671ddea37644e3ff11cb154f15cea2d677976e59
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-17 16:01:27 -0800

    getProducts query working with pagination and count.

--- server/model/models.js
+++ server/model/models.js
@@ -2,23 +2,32 @@ const client = require('../db/index');
 
 // GET /products
 const getProducts= ((page = 1, count = 5) => {
+  let id1 = 1;
+  let id2 = count;
+  if (page > 1) {
+    id1 = ((page - 1) * count) + 1;
+    id2 = id1 + count - 1;
+  }
   client
-    .query('SELECT * FROM products WHERE id > :last_id ORDER BY id LIMIT $1', [count])
+    .query('SELECT * FROM products WHERE id >= $1 and id <= $2', [id1, id2])
     .then((result) => {
       console.log('result: ', result)
     })
     .catch((error) => console.error('error: ', error))
 })
-getProducts();
+// getProducts();
 
-// // GET /products/:product_id
-// const getProductById = ((id) => {
-//   client
-//     .query('SELECT * FROM products WHERE id=$1', [id])
-//     .then((result) => console.log('result: ', result))
-//     .catch((error) => console.error('error: ', error))
-// })
-// getProductById('40344');
+// GET /products/:product_id
+const getProductById = ((id) => {
+  client
+    .query('SELECT * FROM products WHERE id=$1', [id])
+    .then((result) => {
+      console.log('result.rows: ', result.rows)
+    })
+    .catch((error) => console.error('error: ', error))
+})
+getProductById('40344');
+console.log()
 
 // // GET /products/:product_id/related
 // const getRelatedProduct = ((name) => {
