ccb5f9c54cf2cdba88155a31a482bd1583136d7d
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-21 17:03:11 -0800

    Add more tests. Install New Relic. Add to gitignore.

--- testing/stressTests.js
+++ testing/stressTests.js
@@ -8,22 +8,22 @@ export const options = {
       preAllocatedVUs: 500,
       timeUnit: '1s',
       stages: [
-        { duration: '20s', target: 10 }, // below normal load
-        { duration: '50s', target: 10 },
-        { duration: '1m', target: 20 }, // normal load
-        { duration: '1m20s', target: 20 },
-        { duration: '30s', target: 30 }, // around the breaking point
-        { duration: '1m', target: 30 },
-        { duration: '20s', target: 40 }, // beyond the breaking point
-        { duration: '1m', target: 40 },
+        { duration: '20s', target: 100 }, // below normal load
+        { duration: '50s', target: 100 },
+        { duration: '1m', target: 200 }, // normal load
+        { duration: '1m20s', target: 200 },
+        { duration: '30s', target: 300 }, // around the breaking point
+        { duration: '1m', target: 300 },
+        { duration: '20s', target: 400 }, // beyond the breaking point
+        { duration: '1m', target: 400 },
         { duration: '2m', target: 0 }, // scale down. Recovery stage.
       ],
     },
   },
 };
 
-export default function () {
-  const randomProductId = Math.floor(Math.random() * 100000);
+export default () => {
+  const randomProductId = Math.floor(Math.random() * 1000000);
   const BASE_URL = 'http://localhost:3000'; // make sure this is not production
   const responses = http.batch([
     ['GET', `${BASE_URL}/api/products`],
@@ -31,4 +31,5 @@ export default function () {
     ['GET', `${BASE_URL}/api/products/${randomProductId}/related`],
     ['GET', `${BASE_URL}/api/products/${randomProductId}/styles`],
   ]);
-}
+  sleep(1);
+};
