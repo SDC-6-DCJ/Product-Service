332f40117f8c16f6a925119d98fb9066d5463aa4
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-23 14:49:10 -0800

    Add Loader.io environment variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes');
 
-const { SERVER_HOST, LOCAL_PORT } = process.env;
+const { SERVER_HOST, LOCAL_PORT, LOADER_IO } = process.env;
 
 const app = express();
 
@@ -12,6 +12,7 @@ app.use(express.json());
 
 app.use('/api', router);
 app.get('/', (req, res) => res.send('Hello World'));
+app.get(`/${LOADER_IO}`, (req, res) => res.send(`${LOADER_IO}`));
 
 app.listen(LOCAL_PORT);
 console.log(`Server listening at ${SERVER_HOST}:${LOCAL_PORT}`);
